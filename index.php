977f2247ac0b9f51d974ba5dd7958696ebc27aab
ZMejer <zosiacastiel@gmail.com>
2023-10-28 15:24:36 +0200

    testowe polaczenie z baza danych

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php  include 'db_connect.php' ?>
 </head>
 <body>
-    
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+
+    $conn = mysqli_connect($servername, $username, $password);
+
+    if (!$conn) {
+        die('Could not connect: ' . mysqli_error());
+    }
+
+    echo 'Connected successfully';
+    mysql_close($conn); ?>
 </body>
 </html>
