35735d0268725b99b5ddc3e6897823b6a7f2eee4
ZMejer <zosiacastiel@gmail.com>
2023-10-29 03:37:47 +0100

    faw

--- docker_php/php/src/faq.php
+++ docker_php/php/src/faq.php
@@ -19,7 +19,24 @@
             <?php logout_button(); ?>
         </ul>
     </div>
-    <div class="banner">
+    <div class="faq-container">
+        <div class="faq-inner-container"> 
+        <h1><b>Często zadawane pytania</b></h1>
+            <span class="text-bold">Jak mogę zarejestrować się na platformie?</span><br style="clear:both;">
+            <span>Aby zarejestrować się, wystarczy przejść na naszą stronę główną i kliknąć przycisk "Zarejestruj się". Następnie podążaj za krokami rejestracji, podając wymagane informacje, takie jak e-mail i hasło. </span><br><br>
+            <span class="text-bold">Czy korzystanie z platformy jest bezpłatne?</span><br>
+            <span>Tak, korzystanie z naszej platformy jest całkowicie bezpłatne zarówno dla osób niepełnosprawnych, jak i wolontariuszy.</span><br><br>
+            <span class="text-bold">Jak mogę zgłosić potrzebę pomocy?</span><br>
+            <span>Po zalogowaniu na swoje konto, możesz kliknąć przycisk "Dodaj ogłoszenie" i wypełnić formularz, opisując, czym potrzebujesz pomocy. Wprowadź jak najwięcej szczegółów, aby wolontariusze mogli lepiej zrozumieć Twoje potrzeby.</span><br><br>
+            <span class="text-bold">Jak jest zapewnione bezpieczeństwo na platformie?</span><br>
+            <span>Dbamy o bezpieczeństwo naszych użytkowników. Wolontariusze przechodzą weryfikację, a my zachęcamy do korzystania z systemu ocen i opinii. Zawsze warto spotykać się w publicznych miejscach i zachować ostrożność.</span><br><br>
+            <span class="text-bold">Jakie rodzaje zleceń są dostępne na platformie?</span><br>
+            <span>Na naszej platformie znajdziesz różnorodne zlecenia, takie jak zakupy, wyprowadzanie psa na smyczy czy odśnieżanie.</span><br><br>
+            <span class="text-bold">Czy mogę zaoferować pomoc regularnie?</span><br>
+            <span>Oczywiście! Możesz stać się regularnym wolontariuszem i pomagać regularnie, budując dłuższe relacje z osobami potrzebującymi.</span><br><br>
+            <span class="text-bold">Jak mogę kontaktować się z zespołem wsparcia?</span><br>
+            <span>Jeśli masz pytania lub potrzebujesz pomocy, skorzystaj z zakładki "Kontakt" na naszej stronie lub napisz na nasz adres e-mail [adres e-mail wsparcia].</span><br><br>
+        </div>
     </div>
 </body>
 </html>
